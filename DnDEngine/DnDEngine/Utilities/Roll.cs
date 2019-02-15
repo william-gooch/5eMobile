0522425d8ab5b70b73e649602d4c1a04877e7429
William Gooch <william.gooch.wg@gmail.com>
2019-02-15 10:08:34 +0000

    Create private Roll constructor
    This constructor is used for generating new instances of Roll within the class.

--- DnDEngine/DnDEngine/Utilities/Roll.cs
+++ DnDEngine/DnDEngine/Utilities/Roll.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace DnDEngine.Utilities
 {
+    /// <summary>
+    /// The Roll class will hold all utilities for creating dice rolls.
+    /// </summary>
     class Roll
     {
+        private int numberOfSides;
+        private int numberOfDice;
+        private int modifier;
+
+        public int value;
+
+        /// <summary>
+        /// This private constructor is used within the class to make a new Roll instance.
+        /// </summary>
+        /// <param name="numberOfSides">The number of sides each dice has.</param>
+        /// <param name="numberOfDice">The number of dice to roll.</param>
+        /// <param name="modifier">A modifier to add after the roll.</param>
+        private Roll(int numberOfSides, int numberOfDice, int modifier)
+        {
+            this.numberOfSides = numberOfSides;
+            this.numberOfDice = numberOfDice;
+            this.modifier = modifier;
+        }
     }
 }
