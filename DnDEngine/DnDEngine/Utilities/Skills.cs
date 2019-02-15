86dd93b788de4aa745492c69f300e00d6d2f6a91
William Gooch <william.gooch.wg@gmail.com>
2019-02-15 12:47:01 +0000

    Add Skills enum.
    This enum holds all of the skills available to make rolls on in DnD. Each one is set to 1 bitshifted by a certain amount so I can apply OR bitmasks in order to use multiple skills at the same time.

---
+++ DnDEngine/DnDEngine/Utilities/Skills.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Utilities
+{
+    [Flags]
+    enum Skills
+    {
+        None = 0,
+        Acrobatics = 1 << 0,
+        Animal_Handling = 1 << 1,
+        Arcana          = 1 << 2,
+        Athletics       = 1 << 3,
+        Deception       = 1 << 4,
+        History         = 1 << 5,
+        Insight         = 1 << 6,
+        Intimidation    = 1 << 7,
+        Investigation   = 1 << 8,
+        Medicine        = 1 << 9,
+        Nature          = 1 << 10,
+        Perception      = 1 << 11,
+        Performance     = 1 << 12,
+        Persuasion      = 1 << 13,
+        Religion        = 1 << 14,
+        Sleight_Of_Hand = 1 << 15,
+        Stealth         = 1 << 16,
+        Survival        = 1 << 17,
+
+        Strength = Athletics,
+        Dexterity = Acrobatics | Sleight_Of_Hand | Stealth,
+        Constitution = None,
+        Intelligence = Arcana | History | Nature | Religion,
+        Wisdom = Animal_Handling | Insight | Medicine | Perception | Survival,
+        Charisma = Deception | Intimidation | Performance | Persuasion
+    }
+}
