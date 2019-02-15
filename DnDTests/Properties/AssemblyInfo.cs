fbeb9b3a09613342e0dd3f0410fee7128c8a0901
William Gooch <william.gooch.wg@gmail.com>
2019-02-15 12:31:22 +0000

    Create Unit Testing Project.
    Here I created a project to be used for unit testing, and also some simple tests for the Roll class.

---
+++ DnDTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DnDTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DnDTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dbd80dc9-ed62-4bdd-b4e9-29f270e6a4b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DnDTests/RollTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DnDEngine.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DnDTests
+{
+    [TestClass]
+    public class RollTest
+    {
+        [TestMethod]
+        public void TestCreateRoll()
+        {
+            var roll = Roll.D(20);
+            roll.DoRoll();
+            Assert.IsInstanceOfType(roll.value, typeof(int));
+            Console.WriteLine(roll.value);
+        }
+
+        [TestMethod]
+        public void TestModifyRoll()
+        {
+            var roll = Roll.D(20) + 3;
+            roll.DoRoll();
+            Assert.IsTrue(roll.value > 4);
+            Console.WriteLine(roll.value);
+        }
+    }
+}
