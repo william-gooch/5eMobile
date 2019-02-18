4193551860881e6a3429359e828f6efad5458685
William Gooch <william.gooch.wg@gmail.com>
2019-02-18 11:37:59 +0000

    Create Inventory class.
    This class contains a list of items that can be accessed.

---
+++ DnDEngine/DnDEngine/Character/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DnDEngine.Item;
+
+namespace DnDEngine.Character
+{
+    class Inventory
+    {
+        private List<Item> items;
+
+        public Inventory()
+        {
+            items = new List<Item>()
+        }
+    }
+}
