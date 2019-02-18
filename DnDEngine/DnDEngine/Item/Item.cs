a9d9597046b86bd8474f5801dcb3e09a5847bd22
William Gooch <william.gooch.wg@gmail.com>
2019-02-18 11:32:18 +0000

    Create Item class.
    This class is a base to describe all items in the game. Other classes will be able to extend this one, such as weapons, equipment and magic items.

---
+++ DnDEngine/DnDEngine/Item/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Item
+{
+    public class Item
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Notes { get; private set; }
+
+        public Item(string name, string description, string notes)
+        {
+            Name = name;
+            Description = description;
+            Notes = notes;
+        }
+    }
+}
