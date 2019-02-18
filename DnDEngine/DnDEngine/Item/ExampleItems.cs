aeb89639521b91ec8aad3fff365da9e034bafab1
William Gooch <william.gooch.wg@gmail.com>
2019-02-18 11:58:08 +0000

    Add example Items.

---
+++ DnDEngine/DnDEngine/Item/ExampleItems.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Item
+{
+    public static class ExampleItems
+    {
+        public static Item arrow = new Item("Arrow", "An arrow to fire at enemies.", "");
+        public static Item rock = new Item("Rock", "A rock. You might be able to throw it at people.", "");
+    }
+}
