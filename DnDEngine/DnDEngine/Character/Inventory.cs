88132123e5b4f0acebd492c3faf9099a037d486b
William Gooch <william.gooch.wg@gmail.com>
2019-02-18 11:55:38 +0000

    Change Inventory to include quantities.
    Now Inventory should take quantities of items into account.
    I also changed AddItem to check if the item already exists and if it does, just increase the quantity rather than adding a new listing for it.

--- DnDEngine/DnDEngine/Character/Inventory.cs
+++ DnDEngine/DnDEngine/Character/Inventory.cs
@@ -7,21 +7,41 @@ namespace DnDEngine.Character
 {
     class Inventory
     {
-        private List<Item.Item> items;
+        private List<(Item.Item Item, int Quantity)> items;
 
         public Inventory()
         {
-            items = new List<Item.Item>();
+            items = new List<(Item.Item Item, int Quantity)>();
         }
 
-        public void AddItem(Item.Item item)
+        public void AddItem(Item.Item item, int quantity)
         {
-            items.Add(item);
+            // If the item is already in the inventory, then don't add it again.
+            // Just increase the quantity of the existing item in the list.
+            if (ItemInInventory(item.Name))
+            {
+                int index = GetItemIndex(item.Name);
+                items[index] = (items[index].Item, items[index].Quantity + quantity);
+            }
+            else
+            {
+                items.Add((item, quantity));
+            }
         }
 
-        public Item.Item GetItem(string name)
+        public (Item.Item Item, int Quantity) GetItem(string name)
         {
-            return items.Find((Item.Item item) => item.Name == name);
+            return items.Find(((Item.Item Item, int Quantity) tuple) => tuple.Item.Name == name);
+        }
+
+        public int GetItemIndex(string name)
+        {
+            return items.FindIndex(((Item.Item Item, int Quantity) tuple) => tuple.Item.Name == name);
+        }
+
+        public bool ItemInInventory(string name)
+        {
+            return items.Exists(((Item.Item Item, int Quantity) tuple) => tuple.Item.Name == name);
         }
     }
 }
