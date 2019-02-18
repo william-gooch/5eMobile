d7e3895d4d96f1ada3165ac8b5c05195d19d11bc
William Gooch <william.gooch.wg@gmail.com>
2019-02-18 11:16:46 +0000

    Create Character class with constructor.
    This class contains all the information about a specific character in the game (whether it be a player, an NPC or an enemy).
    The six attributes represent the six ability scores the character can have.

---
+++ DnDEngine/DnDEngine/Character/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Character
+{
+    public class Character
+    {
+        public int Strength { get; private set; }
+        public int Dexterity { get; private set; }
+        public int Constitution { get; private set; }
+        public int Intelligence { get; private set; }
+        public int Wisdom { get; private set; }
+        public int Charisma { get; private set; }
+
+        public Character(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
+        {
+            Strength = strength;
+            Dexterity = dexterity;
+            Constitution = constitution;
+            Intelligence = intelligence;
+            Wisdom = wisdom;
+            Charisma = charisma;
+        }
+    }
+}
