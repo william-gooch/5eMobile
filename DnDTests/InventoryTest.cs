9e9e00ce1ff38a62a1bc08553c13090e53e03979
William Gooch <william.gooch.wg@gmail.com>
2019-02-18 12:01:47 +0000

    Add unit test for Inventory.

---
+++ DnDTests/InventoryTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DnDEngine.Character;
+using DnDEngine.Item;
+
+namespace DnDTests
+{
+    [TestClass]
+    public class InventoryTest
+    {
+        [TestMethod]
+        public void TestMultipleItems()
+        {
+            Inventory inv = new Inventory();
+            inv.AddItem(ExampleItems.arrow, 5);
+            inv.AddItem(ExampleItems.rock, 6);
+            inv.AddItem(ExampleItems.arrow, 1);
+
+            Assert.IsTrue(inv.GetItem("Arrow").Quantity == 6);
+            Assert.IsTrue(inv.GetItem("Rock").Quantity == 6);
+        }
+    }
+}
