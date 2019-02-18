2477ab16a359fc26630612d83f4a71b6cd6cc38f
William Gooch <william.gooch.wg@gmail.com>
2019-02-18 11:25:22 +0000

    Add three example Character Presets for enemies.
    Added three presets for characters: goblins, dragons and bandits.

---
+++ DnDEngine/DnDEngine/Character/CharacterPresets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Character
+{
+    public static class CharacterPresets
+    {
+        public static Character goblin = new Character(8, 14, 10, 10, 8, 8);
+        public static Character youngBlackDragon = new Character(19, 14, 17, 12, 11, 15);
+        public static Character bandit = new Character(11, 12, 12, 10, 10, 10);
+    }
+}
