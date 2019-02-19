c02427746d992fff4d98200e1e2eac65eb861517
William Gooch <william.gooch.wg@gmail.com>
2019-02-19 11:22:51 +0000

    Convert all attribute setters in Character class to public .
    Because D&D is such a customizable game, it should be possible to modify any attribute of a Character.

--- DnDEngine/DnDEngine/Character/Character.cs
+++ DnDEngine/DnDEngine/Character/Character.cs
@@ -6,14 +6,14 @@ namespace DnDEngine.Character
 {
     public class Character
     {
-        public string Name { get; private set; }
+        public string Name { get; set; }
 
-        public int Strength { get; private set; }
-        public int Dexterity { get; private set; }
-        public int Constitution { get; private set; }
-        public int Intelligence { get; private set; }
-        public int Wisdom { get; private set; }
-        public int Charisma { get; private set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
 
         public Character(string name, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
         {
