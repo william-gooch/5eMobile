be8c23fceb175d7ea34ed5ab017c0081699e7237
William Gooch <william.gooch.wg@gmail.com>
2019-02-22 13:17:05 +0000

    Add Proficiency attributes to CharacterClass class.

--- DnDEngine/DnDEngine/Character/CharacterClass.cs
+++ DnDEngine/DnDEngine/Character/CharacterClass.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using DnDEngine.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DnDEngine.Character
 {
-    class CharacterClass
+    public class CharacterClass
     {
         public string Name { get; private set; }
         public string Description { get; private set; }
+
+        public Roll HitDice { get; private set; }
+
+        public Skills SkillProficiencies { get; private set; }
+        public SavingThrows SavingThrowProficiencies { get; private set; }
     }
 }
