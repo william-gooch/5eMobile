32eeb7eea4d06745ae0dad30a1f264827e7ff2a0
William Gooch <william.gooch.wg@gmail.com>
2019-02-22 13:13:40 +0000

    Add SavingThrows enum.
    This includes all the possible saving throws that a character would have to take.

---
+++ DnDEngine/DnDEngine/Utilities/SavingThrows.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Utilities
+{
+    [Flags]
+    public enum SavingThrows
+    {
+        Strength = 1 << 0,
+        Dexterity = 1 << 1,
+        Constitution = 1 << 2,
+        Intelligence = 1 << 3,
+        Wisdom = 1 << 4,
+        Charisma = 1 << 5
+    }
+}
