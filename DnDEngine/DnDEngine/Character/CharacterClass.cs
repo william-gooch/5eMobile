3781869e7b02511257213ce16586376514110a38
William Gooch <william.gooch.wg@gmail.com>
2019-02-22 13:08:15 +0000

    Add CharacterClass Class

---
+++ DnDEngine/DnDEngine/Character/CharacterClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Character
+{
+    class CharacterClass
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+    }
+}
