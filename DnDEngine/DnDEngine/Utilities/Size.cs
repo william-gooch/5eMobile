9a8131ea8368ff3819eabe1dee619c2f1852fc64
William Gooch <william.gooch.wg@gmail.com>
2019-02-22 13:18:28 +0000

    Create Size enum.
    Contains the possible sizes for a character to be.

---
+++ DnDEngine/DnDEngine/Utilities/Size.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Utilities
+{
+    public enum Size
+    {
+        Tiny,
+        Small,
+        Medium,
+        Large,
+        Huge,
+        Gargantuan
+    }
+}
