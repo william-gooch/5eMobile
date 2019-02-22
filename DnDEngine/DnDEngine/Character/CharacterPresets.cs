15bdbcd0b98fe24bdb60a47caef4bb8f17c84ad4
William Gooch <william.gooch.wg@gmail.com>
2019-02-22 12:11:01 +0000

    Fix example character presets.
    Add the new attributes to the character presets.

--- DnDEngine/DnDEngine/Character/CharacterPresets.cs
+++ DnDEngine/DnDEngine/Character/CharacterPresets.cs
@@ -6,8 +6,8 @@ namespace DnDEngine.Character
 {
     public static class CharacterPresets
     {
-        public static Character goblin = new Character(8, 14, 10, 10, 8, 8);
-        public static Character youngBlackDragon = new Character(19, 14, 17, 12, 11, 15);
-        public static Character bandit = new Character(11, 12, 12, 10, 10, 10);
+        public static Character goblin = new Character("Goblin", 8, 14, 10, 10, 8, 8, 15, 7);
+        public static Character youngBlackDragon = new Character("Young Black Dragon", 19, 14, 17, 12, 11, 15, 18, 127);
+        public static Character bandit = new Character("Bandit", 11, 12, 12, 10, 10, 10, 12, 11);
     }
 }
