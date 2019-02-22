439a7ae08684ef1520ba697e66253a33a87cb83e
William Gooch <william.gooch.wg@gmail.com>
2019-02-22 13:21:40 +0000

    Create CharacterRace class.
    This class defines the race that a player character is. This will have its own traits and actions as well.

---
+++ DnDEngine/DnDEngine/Character/CharacterRace.cs
@@ -0,0 +1,15 @@
+﻿using DnDEngine.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Character
+{
+    class CharacterRace
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Size Size { get; private set; }
+        public int Speed { get; private set; }
+    }
+}
