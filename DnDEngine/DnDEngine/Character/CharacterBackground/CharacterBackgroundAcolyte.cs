d3d41c0127d119ed51f111eb10f1cb5fa22dbec1
William Gooch <william.gooch.wg@gmail.com>
2019-02-23 15:47:26 +0000

    Add CharacterBackgroundAcolyte as an example background.
    This inherits from CharacterBackground and implements both properties in it so far.

---
+++ DnDEngine/DnDEngine/Character/CharacterBackground/CharacterBackgroundAcolyte.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Character.CharacterBackground
+{
+    public class CharacterBackgroundAcolyte : CharacterBackground
+    {
+        public override string Name { get; } = "Acolyte";
+        public override string Description { get; } = "You have spent your life in the service of a temple to a specific god or pantheon of gods.";
+    }
+}
