dd1fdfc334e212c7f81b63964c582645d89977e1
William Gooch <william.gooch.wg@gmail.com>
2019-02-23 16:14:02 +0000

    Change Character presets to use CharacterPreset class.
    The constructor should now be CharacterPreset as that is how a new Non-Player Character preset is made.

--- DnDEngine/DnDEngine/Character/CharacterPresets.cs
+++ DnDEngine/DnDEngine/Character/CharacterPresets.cs
@@ -7,8 +7,8 @@ namespace DnDEngine.Character
 {
     public static class CharacterPresets
     {
-        public static Character goblin = new Character("Goblin", new AbilityScores(8, 14, 10, 10, 8, 8), 15, 7);
-        public static Character youngBlackDragon = new Character("Young Black Dragon", new AbilityScores(19, 14, 17, 12, 11, 15), 18, 127);
-        public static Character bandit = new Character("Bandit", new AbilityScores(11, 12, 12, 10, 10, 10), 12, 11);
+        public static Character goblin = new CharacterPreset("Goblin", new AbilityScores(8, 14, 10, 10, 8, 8), 15, 7);
+        public static Character youngBlackDragon = new CharacterPreset("Young Black Dragon", new AbilityScores(19, 14, 17, 12, 11, 15), 18, 127);
+        public static Character bandit = new CharacterPreset("Bandit", new AbilityScores(11, 12, 12, 10, 10, 10), 12, 11);
     }
 }
