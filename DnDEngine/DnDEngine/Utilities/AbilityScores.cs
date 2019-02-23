f0c83d56530c600676a151ab745e683e1fb82ca2
William Gooch <william.gooch.wg@gmail.com>
2019-02-23 14:51:04 +0000

    Create AbilityScores class.
    This class holds the six Ability Scores a character has.
    The six separate variables will be refactored into this class instead to conform to SRP.

---
+++ DnDEngine/DnDEngine/Utilities/AbilityScores.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Utilities
+{
+    class AbilityScores
+    {
+        public int Strength { get; }
+        public int Dexterity { get; }
+        public int Constitution { get; }
+        public int Intelligence { get; }
+        public int Wisdom { get; }
+        public int Charisma { get; }
+
+        public AbilityScores(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
+        {
+            Strength = strength;
+            Dexterity = dexterity;
+            Constitution = constitution;
+            Intelligence = intelligence;
+            Wisdom = wisdom;
+            Charisma = charisma;
+        }
+    }
+}
