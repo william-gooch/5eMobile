cfa6d2e2c5fe4e7cef9837eaee0a6d80bb4eb199
William Gooch <william.gooch.wg@gmail.com>
2019-02-23 15:01:22 +0000

    Refactor classes to use AbilityScores instead of six attributes.

--- DnDEngine/DnDEngine/Character/Character.cs
+++ DnDEngine/DnDEngine/Character/Character.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DnDEngine.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,32 +9,22 @@ namespace DnDEngine.Character
     {
         public string Name { get; set; }
 
-        public int Strength { get; set; }
-        public int Dexterity { get; set; }
-        public int Constitution { get; set; }
-        public int Intelligence { get; set; }
-        public int Wisdom { get; set; }
-        public int Charisma { get; set; }
+        public AbilityScores BaseAbilityScores { get; set; }
 
         public int ArmorClass { get; set; }
         public int MaximumHitPoints { get; set; }
 
-        public Character(string name, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma, int armorClass, int maximumHitPoints)
+        public Character(string name, AbilityScores baseAbilityScores, int armorClass, int maximumHitPoints)
         {
             Name = name;
-            Strength = strength;
-            Dexterity = dexterity;
-            Constitution = constitution;
-            Intelligence = intelligence;
-            Wisdom = wisdom;
-            Charisma = charisma;
+            BaseAbilityScores = baseAbilityScores;
             ArmorClass = armorClass;
             MaximumHitPoints = maximumHitPoints;
         }
 
         public Character Clone()
         {
-            return new Character(Name, Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma, ArmorClass, MaximumHitPoints);
+            return new Character(Name, BaseAbilityScores, ArmorClass, MaximumHitPoints);
         }
     }
 }
--- DnDEngine/DnDEngine/Character/CharacterPresets.cs
+++ DnDEngine/DnDEngine/Character/CharacterPresets.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DnDEngine.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +7,8 @@ namespace DnDEngine.Character
 {
     public static class CharacterPresets
     {
-        public static Character goblin = new Character("Goblin", 8, 14, 10, 10, 8, 8, 15, 7);
-        public static Character youngBlackDragon = new Character("Young Black Dragon", 19, 14, 17, 12, 11, 15, 18, 127);
-        public static Character bandit = new Character("Bandit", 11, 12, 12, 10, 10, 10, 12, 11);
+        public static Character goblin = new Character("Goblin", new AbilityScores(8, 14, 10, 10, 8, 8), 15, 7);
+        public static Character youngBlackDragon = new Character("Young Black Dragon", new AbilityScores(19, 14, 17, 12, 11, 15), 18, 127);
+        public static Character bandit = new Character("Bandit", new AbilityScores(11, 12, 12, 10, 10, 10), 12, 11);
     }
 }
