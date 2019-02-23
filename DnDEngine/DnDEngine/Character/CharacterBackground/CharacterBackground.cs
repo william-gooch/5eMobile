60159bc6250defe2985a55965d445d24733825a8
William Gooch <william.gooch.wg@gmail.com>
2019-02-23 15:43:51 +0000

    Create CharacterBackground class.
    This class contains the information required for the character's background. At the moment, this is just a name and a description. This is all abstract, just like the CharacterRace and CharacterClass classes.

---
+++ DnDEngine/DnDEngine/Character/CharacterBackground/CharacterBackground.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Character.CharacterBackground
+{
+    public abstract class CharacterBackground
+    {
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+    }
+}
