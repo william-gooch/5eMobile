8b4a3bde6ec72bcf804a9292c730281d7e860851
William Gooch <william.gooch.wg@gmail.com>
2019-02-23 15:31:48 +0000

    Add CharacterRaceDragonborn as an example race.
    This inherits from CharacterRace and implements the properties defined in there. Each of them has a static, immutable value as they will not change from character to character.

---
+++ DnDEngine/DnDEngine/Character/CharacterRace/CharacterRaceDragonborn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DnDEngine.Utilities;
+
+namespace DnDEngine.Character.CharacterRace
+{
+    public class CharacterRaceDragonborn : CharacterRace
+    {
+        public override string Name { get; } = "Dragonborn";
+        public override string Description { get; } = "Born of dragons, as their name proclaims, the dragonborn walk proudly through a world that greets them with fearful incomprehension.";
+
+        public override Size Size { get; } = Size.Medium;
+        public override int Speed { get; } = 30;
+
+        public override AbilityScores AbilityScoresMod { get; } = new AbilityScores(2, 0, 0, 0, 0, 1);
+    }
+}
