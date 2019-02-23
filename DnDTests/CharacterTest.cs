79318053feb6730bf70b430a096285c7d3445fdc
William Gooch <william.gooch.wg@gmail.com>
2019-02-23 17:55:47 +0000

    Add test class for Character creation.
    This includes two unit tests, one which checks that a player character is initialised correctly and has the right armor class and max hit points, and the other which checks that a character preset is initialised correctly.

---
+++ DnDTests/CharacterTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DnDEngine.Character;
+using DnDEngine.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DnDTests
+{
+    [TestClass]
+    public class CharacterTest
+    {
+        [TestMethod]
+        public void TestPlayerCharacter()
+        {
+            PlayerCharacter myCharacter = new PlayerCharacter(
+                "Jorgen Windhelm",
+                new AbilityScores(16, 9, 15, 11, 13, 14),
+                new DnDEngine.Character.CharacterRace.CharacterRaceDragonborn(),
+                new DnDEngine.Character.CharacterClass.CharacterClassBarbarian(),
+                new DnDEngine.Character.CharacterBackground.CharacterBackgroundAcolyte()
+            );
+            Assert.AreEqual(myCharacter.Name, "Jorgen Windhelm");
+            Assert.AreEqual(myCharacter.Race.Name, "Dragonborn");
+            Assert.AreEqual(myCharacter.ArmorClass, 9);
+            Assert.AreEqual(myCharacter.MaximumHitPoints, 12);
+        }
+
+        [TestMethod]
+        public void TestCharacterPreset()
+        {
+            Character myGoblin = CharacterPresets.goblin.Clone();
+            Assert.AreEqual(myGoblin.ArmorClass, 15);
+            Assert.AreEqual(myGoblin.Name, "Goblin");
+        }
+    }
+}
