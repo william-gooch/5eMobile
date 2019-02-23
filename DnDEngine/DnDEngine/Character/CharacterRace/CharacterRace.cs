cd9bcc902ec84c213f76071ab261529b4aa109b8
William Gooch <william.gooch.wg@gmail.com>
2019-02-23 15:20:25 +0000

    Move CharacterClass and CharacterRace to their own folders.
    This is part of a restructure I'm doing where all classes that inherit from either of these bases will be in this folder.

--- DnDEngine/DnDEngine/Character/CharacterRace/CharacterRace.cs
+++ DnDEngine/DnDEngine/Character/CharacterRace/CharacterRace.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace DnDEngine.Character
+namespace DnDEngine.Character.CharacterRace
 {
     public class CharacterRace
     {
