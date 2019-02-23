17dbe269461c9e934ec98d7b17e4d9e903343d7d
William Gooch <william.gooch.wg@gmail.com>
2019-02-23 15:07:49 +0000

    Make CharacterRace a public class.

--- DnDEngine/DnDEngine/Character/CharacterRace.cs
+++ DnDEngine/DnDEngine/Character/CharacterRace.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace DnDEngine.Character
 {
-    class CharacterRace
+    public class CharacterRace
     {
         public string Name { get; private set; }
         public string Description { get; private set; }
