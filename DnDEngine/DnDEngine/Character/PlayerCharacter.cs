f637562784935e678e4459722bcf81a7f85fdd6e
William Gooch <william.gooch.wg@gmail.com>
2019-02-23 15:52:34 +0000

    Add PlayerCharacter class.
    This class extends the Character class in a way that allows for classes, races, and backgrounds to be chosen by the player.

---
+++ DnDEngine/DnDEngine/Character/PlayerCharacter.cs
@@ -0,0 +1,15 @@
+﻿using DnDEngine.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDEngine.Character
+{
+    public class PlayerCharacter : Character
+    {
+        public PlayerCharacter(string name, AbilityScores baseAbilityScores, int armorClass, int maximumHitPoints) : base(name, baseAbilityScores, armorClass, maximumHitPoints)
+        {
+
+        }
+    }
+}
