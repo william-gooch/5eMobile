7cee2e2c701396c18a925e12612844660b5a4afd
William Gooch <william.gooch.wg@gmail.com>
2019-02-23 15:38:23 +0000

    Add CharacterClassBarbarian as an example class.
    This implements all of the properties from CharacterClass except for SkillProficiencies. The reason that it's left unimplemented as it requires a choice from the user creating the character, so I chose to leave it unimplemented until I develop a little more of the character creator.

---
+++ DnDEngine/DnDEngine/Character/CharacterClass/CharacterClassBarbarian.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DnDEngine.Utilities;
+
+namespace DnDEngine.Character.CharacterClass
+{
+    public class CharacterClassBarbarian : CharacterClass
+    {
+        public override string Name { get; } = "Barbarian";
+        public override string Description { get; } = "Barbarians come alive in the chaos of combat.They can enter a berserk state where rage takes over, giving them superhuman strength and resilience.";
+
+        public override Roll HitDice { get; } = Roll.D(12);
+
+        public override Skills SkillProficiencies => throw new NotImplementedException();
+        public override SavingThrows SavingThrowProficiencies { get; } = SavingThrows.Strength | SavingThrows.Constitution;
+    }
+}
