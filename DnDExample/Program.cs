9e81c475688321bcb73e08448493ba7e610ddbe6
William Gooch <william.gooch.wg@gmail.com>
2019-03-04 21:40:11 +0000

    Create new DnDExample project.
    This is a simple Console project to help show off my progress through the app.
    At the moment it is just a blank console app.

---
+++ DnDExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DnDEngine;
+using DnDEngine.Character;
+using DnDEngine.Utilities;
+
+namespace DnDExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
