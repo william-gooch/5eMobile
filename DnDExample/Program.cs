dd2f241944ddc46d735e1168e7ca12d02f5d9440
William Gooch <william.gooch.wg@gmail.com>
2019-03-04 21:41:01 +0000

    Add basic Character info to DnDExample.
    This displays basic information about a player character.
    This is like a text-based version of the eventual character sheet screen in the real app.

--- DnDExample/Program.cs
+++ DnDExample/Program.cs
@@ -14,7 +14,21 @@ namespace DnDExample
     {
         static void Main(string[] args)
         {
-            
+            var myPlayerCharacter = new PlayerCharacter("Lukan Volgen", new AbilityScores(20, 9, 16, 9, 13, 12),
+                new DnDEngine.Character.CharacterRace.CharacterRaceDragonborn(),
+                new DnDEngine.Character.CharacterClass.CharacterClassBarbarian(),
+                new DnDEngine.Character.CharacterBackground.CharacterBackgroundAcolyte());
+            Console.WriteLine($"--- {myPlayerCharacter.Name.ToUpper()} ---");
+            Console.WriteLine($"{myPlayerCharacter.Race.Name} {myPlayerCharacter.Class.Name}, {myPlayerCharacter.Background.Name}");
+            Console.WriteLine($"HP: {myPlayerCharacter.MaximumHitPoints}");
+            Console.WriteLine($"Armor Class: {myPlayerCharacter.ArmorClass}");
+            Console.WriteLine($"STR: {myPlayerCharacter.BaseAbilityScores.Strength} ({myPlayerCharacter.BaseAbilityScores.StrengthMod})");
+            Console.WriteLine($"DEX: {myPlayerCharacter.BaseAbilityScores.Dexterity} ({myPlayerCharacter.BaseAbilityScores.DexterityMod})");
+            Console.WriteLine($"CON: {myPlayerCharacter.BaseAbilityScores.Constitution} ({myPlayerCharacter.BaseAbilityScores.ConstitutionMod})");
+            Console.WriteLine($"INT: {myPlayerCharacter.BaseAbilityScores.Intelligence} ({myPlayerCharacter.BaseAbilityScores.IntelligenceMod})");
+            Console.WriteLine($"WIS: {myPlayerCharacter.BaseAbilityScores.Wisdom} ({myPlayerCharacter.BaseAbilityScores.WisdomMod})");
+            Console.WriteLine($"CHA: {myPlayerCharacter.BaseAbilityScores.Charisma} ({myPlayerCharacter.BaseAbilityScores.CharismaMod})");
+            Console.ReadKey();
         }
     }
 }
