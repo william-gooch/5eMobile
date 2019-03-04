ada2c7c20c5461228874075792d2a7cdac84a402
William Gooch <william.gooch.wg@gmail.com>
2019-03-04 21:45:42 +0000

    Add traits info to DnDExample.
    Add a list of traits for the given character to the example project.

--- DnDExample/Program.cs
+++ DnDExample/Program.cs
@@ -22,12 +22,19 @@ namespace DnDExample
             Console.WriteLine($"{myPlayerCharacter.Race.Name} {myPlayerCharacter.Class.Name}, {myPlayerCharacter.Background.Name}");
             Console.WriteLine($"HP: {myPlayerCharacter.MaximumHitPoints}");
             Console.WriteLine($"Armor Class: {myPlayerCharacter.ArmorClass}");
+
             Console.WriteLine($"STR: {myPlayerCharacter.BaseAbilityScores.Strength} ({myPlayerCharacter.BaseAbilityScores.StrengthMod})");
             Console.WriteLine($"DEX: {myPlayerCharacter.BaseAbilityScores.Dexterity} ({myPlayerCharacter.BaseAbilityScores.DexterityMod})");
             Console.WriteLine($"CON: {myPlayerCharacter.BaseAbilityScores.Constitution} ({myPlayerCharacter.BaseAbilityScores.ConstitutionMod})");
             Console.WriteLine($"INT: {myPlayerCharacter.BaseAbilityScores.Intelligence} ({myPlayerCharacter.BaseAbilityScores.IntelligenceMod})");
             Console.WriteLine($"WIS: {myPlayerCharacter.BaseAbilityScores.Wisdom} ({myPlayerCharacter.BaseAbilityScores.WisdomMod})");
             Console.WriteLine($"CHA: {myPlayerCharacter.BaseAbilityScores.Charisma} ({myPlayerCharacter.BaseAbilityScores.CharismaMod})");
+            Console.WriteLine("\nTRAITS:\n");
+            myPlayerCharacter.CharacterTraits.ForEach((trait) =>
+            {
+                Console.WriteLine(trait.Name.ToUpper() + ":\n" + trait.Description);
+            });
+
             Console.ReadKey();
         }
     }
