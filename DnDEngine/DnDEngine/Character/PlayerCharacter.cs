16967f2aede935220456960e1db57f247f358231
William Gooch <william.gooch.wg@gmail.com>
2019-05-21 12:17:54 +0100

    Improve formatting of PlayerCharacter traits accessor.

--- DnDEngine/DnDEngine/Character/PlayerCharacter.cs
+++ DnDEngine/DnDEngine/Character/PlayerCharacter.cs
@@ -29,7 +29,10 @@ namespace DnDEngine.Character
         {
             get
             {
-                return base.CharacterTraits.Concat(Class.CharacterTraits).Concat(Race.CharacterTraits).Concat(Background.CharacterTraits).ToList();
+                return base.CharacterTraits
+                    .Concat(Class.CharacterTraits)
+                    .Concat(Race.CharacterTraits)
+                    .Concat(Background.CharacterTraits).ToList();
             }
         }
 
