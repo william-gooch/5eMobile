024caab6527281a05a50d6b592360720c6caa76b
William Gooch <william.gooch.wg@gmail.com>
2019-09-23 16:03:51 +0100

    Change label to title of main page.

--- DnDApp/DnDApp/MainPage.xaml.cs
+++ DnDApp/DnDApp/MainPage.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace DnDApp
 {
@@ -11,6 +8,7 @@ namespace DnDApp
     {
         public MainPage()
         {
+            Title = "Dungeons and Dragons Companion";
             InitializeComponent();
         }
     }
