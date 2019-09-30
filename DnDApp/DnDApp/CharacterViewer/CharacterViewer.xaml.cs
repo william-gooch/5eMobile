f1b59a29c27e4df7677e57b3154eff7b49eaaf39
william-gooch <gooch.w@etoncollege.org.uk>
2019-09-30 13:12:20 +0100

    Change character viewer to use static property.

--- DnDApp/DnDApp/CharacterViewer/CharacterViewer.xaml.cs
+++ DnDApp/DnDApp/CharacterViewer/CharacterViewer.xaml.cs
@@ -7,11 +7,21 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using DnDEngine.Utilities;
+
 namespace DnDApp.CharacterViewer
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CharacterViewer : ContentPage
     {
+        private AbilityScores abilityScores = new AbilityScores(20, 19, 18, 17, 16, 15);
+
+        public AbilityScores AbilityScores
+        {
+            get { return abilityScores; }
+            set { abilityScores = value; }
+        }
+
         public CharacterViewer()
         {
             InitializeComponent();
