8f9d45181e3d9b889ecb13a4353ddf40e65219c7
William Gooch <gooch.w@etoncollege.org.uk>
2019-11-10 22:43:20 +0000

    Enable CollectionView in both Android and iOS projects.

--- DnDApp/DnDApp.Android/MainActivity.cs
+++ DnDApp/DnDApp.Android/MainActivity.cs
@@ -18,6 +18,7 @@ namespace DnDApp.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
