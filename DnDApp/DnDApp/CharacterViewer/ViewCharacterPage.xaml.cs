dc94216cbb1a754bdadf9a522ef1229e4c88c1a3
William Gooch <gooch.w@etoncollege.org.uk>
2019-11-10 22:18:10 +0000

    Create tabbed page, hopefully this works on iOS too

---
+++ DnDApp/DnDApp/CharacterViewer/ViewCharacterPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using DnDEngine.Character;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DnDApp.CharacterViewer
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewCharacterPage : TabbedPage
+    {
+        public ViewCharacterPage(PlayerCharacter character)
+        {
+            InitializeComponent();
+
+            Children.Add(new CharacterViewer(character));
+            Children.Add(new CharacterTraits());
+        }
+    }
+}
--- DnDApp/DnDApp/MainPage.xaml.cs
+++ DnDApp/DnDApp/MainPage.xaml.cs
@@ -21,7 +21,7 @@ namespace DnDApp
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new CharacterViewer.CharacterViewer(character));
+            await Navigation.PushAsync(new CharacterViewer.ViewCharacterPage(character));
         }
     }
 }
