301eed4d3c33766f8f615a4fa50a11e64766252d
William Gooch <gooch.w@etoncollege.org.uk>
2019-11-10 22:50:02 +0000

    Add CollectionView to the traits page to display all the traits.

--- DnDApp/DnDApp/CharacterViewer/CharacterTraits.xaml.cs
+++ DnDApp/DnDApp/CharacterViewer/CharacterTraits.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DnDEngine.Character;
+using DnDEngine.Utilities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +18,32 @@ namespace DnDApp.CharacterViewer
         public CharacterTraits()
         {
             InitializeComponent();
+        }
+    }
+
+    public class CharacterTraitsModel : BindableObject
+    {
+        private PlayerCharacter character;
+
+        public PlayerCharacter Character
+        {
+            get
+            {
+                if (character == null)
+                {
+                    Character = new PlayerCharacter("Jorgen Windhelm",
+                        new AbilityScores(0, 0, 0, 0, 0, 0),
+                        new CharacterRace.Dragonborn(),
+                        new CharacterClass.Barbarian(),
+                        new CharacterBackground.Acolyte());
+                }
+                return character;
+            }
+            set
+            {
+                character = value;
+                OnPropertyChanged("Character");
+            }
         }
     }
 }
