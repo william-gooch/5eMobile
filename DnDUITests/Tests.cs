798834dc71e05970effb754f59bb90fbde270df2
William Gooch <gooch.w@etoncollege.org.uk>
2019-11-17 22:34:11 +0000

    Add a Dice Rolling test.

--- DnDUITests/Tests.cs
+++ DnDUITests/Tests.cs
@@ -68,5 +68,27 @@ namespace DnDUITests
                 .Child().Text("")
             ));
         }
+
+        [Test]
+        public void CharacterRollsDice()
+        {
+            app.Tap(c => c.Marked("Characters"));
+            // should tap the first ability score view, i.e. strength
+            app.Tap(c => c.Marked("AbilityScoreView"));
+            Assert.IsNotEmpty(app.Query(c => c.Marked("Result")));
+            Assert.IsEmpty(app.Query(c => c.Marked("Result").Text("")));
+            Assert.IsEmpty(app.Query(c => c.Marked("Description").Text("")));
+
+            // this tries to minimize the chance that the test fails by chance
+            // if the value is coincidentally the same both times it will try again.
+            for (int tries = 5; tries >= 0; tries--) {
+                string previousValue = app.Query("Result").First().Text;
+                app.Tap(c => c.Marked("Re-roll"));
+                string currentValue = app.Query("Result").First().Text;
+                if (previousValue != currentValue)
+                    Assert.Pass();
+            }
+            Assert.Fail("Result does not change after 5 tries");
+        }
     }
 }
