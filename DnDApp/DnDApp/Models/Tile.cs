2dafdb993d9f2690604f3f3614455a088acc3140
William Gooch <gooch.w@etoncollege.org.uk>
2019-12-06 09:21:24 +0000

    Add classes for Tile and Tileset.

---
+++ DnDApp/DnDApp/Models/Tile.cs
@@ -0,0 +1,16 @@
+﻿using Plugin.CloudFirestore.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDApp.Models
+{
+    public class Tile
+    {
+        [MapTo("name")]
+        public string Name { get; set; }
+
+        [MapTo("imageUrl")]
+        public string ImageURL { get; set; }
+    }
+}
---
+++ DnDApp/DnDApp/Models/Tileset.cs
@@ -0,0 +1,30 @@
+﻿using Plugin.CloudFirestore.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DnDApp.Models
+{
+    public class Tileset : DatabaseModel
+    {
+        [MapTo("name")]
+        public string Name { get; set; }
+
+        [MapTo("tiles")]
+        public List<Tile> Tiles { get; set; }
+
+        public Tile this[string name]
+        {
+            get => (from tile in Tiles
+                    where tile.Name == name
+                    select tile).First();
+        }
+
+        public Tile this[int index]
+        {
+            get => Tiles[index];
+            set => Tiles[index] = value;
+        }
+    }
+}
